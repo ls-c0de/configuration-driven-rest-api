c3c9635cedb88c1b320b2353a7897e01b28ddd8c
ls-c0de <137232009+ls-c0de@users.noreply.github.com>
2025-11-17 20:33:03 +0100

    Added: Raw Notify Watcher to track changes + read_toml to read the toml file.
    Using them together causes a bug. Next commit will try to fix that by using a poll watcher.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 ## To-Do
+- Step 1: Build Async TOML File reader which parses it into structs
 - Build generic Warp routes which are getting constructed using the toml config files
 - Build generic db methods
 - dockerize it for simple deployment
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use serde::Deserialize;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher, event};
+use notify::event::{EventKind, ModifyKind};
+use notify::*;
+use std::{path::Path, fs, time::Duration};
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Config {
+    field: Vec<Field>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Field {
+    name: String,
+    datatype: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("configs/shop.toml");
+
+    println!("Watching {:?}", path);
+    
+    if let Err(error) = watch(path) {
+        println!("Error: {error:?}");
+    }
+}
+
+fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let mut watcher = RecommendedWatcher::new(tx, notify::Config::default())?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(path.as_ref(), RecursiveMode::NonRecursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => {
+                // 1. Nur echte File-Write Events akzeptieren
+                if !matches!(event.kind, EventKind::Modify(ModifyKind::Data(_))) {
+                    read_toml();
+                    
+                };
+            },
+            Err(error) => println!("Error: {error:?}"),
+        }
+    }
+
+    Ok(())
+}
+
+fn read_toml() {
+    let data = fs::read_to_string("configs/shop.toml").expect("");
+    let config: Config = toml::from_str(&data).expect("");
+
+    println!("{:?}", config)
 }
