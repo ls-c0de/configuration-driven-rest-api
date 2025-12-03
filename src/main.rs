9316374161b5fb6d88770a1cf3bb642552a42f8f
ls-c0de <137232009+ls-c0de@users.noreply.github.com>
2025-12-03 17:27:30 +0100

    Reafactoring loading config into seperat file

--- src/main.rs
+++ src/main.rs
@@ -37,25 +37,6 @@ fn build_routes()
     hi.or(bye)
 }
 
-#[allow(dead_code)]
-#[derive(Debug, Deserialize)]
-struct Config {
-    greeting: Vec<Greeting>,
-}
-
-#[allow(dead_code)]
-#[derive(Debug, Deserialize)]
-struct Greeting {
-    name: String,
-    text: String,
-}
-
-#[allow(dead_code)]
-fn load_config() -> Config {
-    let s = std::fs::read_to_string("configs/easy.toml");
-    toml::from_str(&s.expect("Error")).unwrap()
-}
-
 async fn serve<F>(routes: F)
 where
     F: Filter + Clone + Send + Sync + 'static,
