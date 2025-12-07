7fc7062cef6fd5c7abf9e73291a1b5daa15cb36a
ls-c0de <137232009+ls-c0de@users.noreply.github.com>
2025-12-07 19:08:53 +0100

    renamednotify to io for better explanation

--- src/io/loader.rs
+++ src/io/loader.rs
@@ -1,16 +1,13 @@
-#[allow(dead_code)]
 fn load_config() -> Config {
     let s = std::fs::read_to_string("configs/easy.toml");
     toml::from_str(&s.expect("Error")).unwrap()
 }
 
-#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct Config {
     greeting: Vec<Greeting>,
 }
 
-#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct Greeting {
     name: String,
---
+++ src/io/tracker.rs
@@ -0,0 +1,70 @@
+use serde::Deserialize;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher, event};
+use notify::event::{EventKind, ModifyKind};
+use notify::*;
+use std::{path::Path, fs, time::Duration};
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Config {
+    field: Vec<Field>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Field {
+    name: String,
+    datatype: String,
+}
+
+#[allow(dead_code)]
+fn track_changes_to_configs() {
+    let path = Path::new("configs/shop.toml");
+
+    println!("Watching {:?}", path);
+    
+    if let Err(error) = watch(path) {
+        println!("Error: {error:?}");
+    }
+}
+
+fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let mut watcher = PollWatcher::new(
+        tx, 
+        notify::Config::default().with_poll_interval(Duration::from_secs(1)))?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(path.as_ref(), RecursiveMode::NonRecursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => {
+                println!("Event: {event:?}");
+             track_toml();
+            },
+            Err(error) => println!("Error: {error:?}"),
+        }
+    }
+
+    Ok(())
+}
+
+fn track_toml() {
+    let data = fs::read_to_string("configs/shop.toml").expect("");
+    let config: Config = match toml::from_str(&data) {
+        Ok(cfg) => cfg,
+        Err(e) => {
+            println!("Failed to read! {:?}", e);
+            Config {
+                field: vec![Field {name : "None".to_string(), datatype: "None".to_string()}], // Default Value
+            }
+        }
+    };
+
+    println!("{:?}", config)
+}
