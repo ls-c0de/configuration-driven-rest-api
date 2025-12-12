68e2130ca622671004ca993b4a12449764cbbef5
ls-c0de <137232009+ls-c0de@users.noreply.github.com>
2025-12-12 18:43:53 +0100

    added dummy for response handling

--- src/api/networking/filter.rs
+++ src/api/networking/filter.rs
@@ -1,20 +1,29 @@
 use std::future::ready;
 use warp::{Filter};
+use warp::http;
 
 pub fn build_filter(base: String, paths: Vec<String>) -> impl Filter<Extract = (String,), Error = warp::Rejection> + Clone {
     warp::path(base)
-        .and(warp::get())
         .and(warp::path::tail())
-        .and_then(move |tail: warp::path::Tail| {
-            let path = tail.as_str().to_string();
+        .and(warp::method())
+        .and_then(move |tail: warp::path::Tail, method: http::Method| {
+            let req = Dummy {
+                path: tail.as_str().to_string(),
+                method: method.clone(),
+            };
 
-            if paths.contains(&path) {
-                ready(Ok(path))
+            if paths.contains(&req.path) {
+                ready(Ok(req))
             } else {
                 ready(Err(warp::reject::not_found()))
             }
         })
-    .map(|path: String| {
-        format!("GET on this: {}", path) // Method invokation for db here
+    .map(|req: Dummy| {
+        format!("{} on this: {}", req.method, req.path) // Method invokation for db here
     })
+}
+
+struct Dummy {
+    path: String,
+    method: http::Method,
 }
