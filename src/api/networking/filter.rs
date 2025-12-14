e223ef3c1069369f85708b0ee6db0d9968165a76
ls-c0de <137232009+ls-c0de@users.noreply.github.com>
2025-12-14 14:48:32 +0100

    Added functions depending on http method

--- src/api/networking/filter.rs
+++ src/api/networking/filter.rs
@@ -2,6 +2,7 @@ use std::future::ready;
 use warp::reply::Json;
 use warp::{Filter};
 use warp::http;
+use crate::api::networking::methods;
 
 pub fn build_3_step_filter(base: String, paths: Vec<String>) -> impl Filter<Extract = (Json,), Error = warp::Rejection> + Clone {
     warp::path(base)
@@ -10,9 +11,6 @@ pub fn build_3_step_filter(base: String, paths: Vec<String>) -> impl Filter<Extr
         .and_then(move |tail: warp::path::Tail, method: http::Method| {
            request_handler(paths.clone(), tail, method)
         })
-    // .map(|reply: warp::reply::Json| {
-    //     response_handler(reply)
-    // })
 }
 
 fn request_handler(paths: Vec<String>, tail: warp::path::Tail, method: http::Method) -> impl warp::Future<Output = Result<warp::reply::Json, warp::Rejection>> {
@@ -21,27 +19,45 @@ fn request_handler(paths: Vec<String>, tail: warp::path::Tail, method: http::Met
                 method: method.clone(),
             };
 
-            if paths.contains(&req.path) {
-                ready(Ok(response_handler(req)))
-                //ready(Ok(warp::reply::json(&req)))
-            } else {
-                ready(Err(warp::reject::not_found()))
+            match response_handler(req, paths) {
+                Ok(json) => ready(Ok(json)),
+                Err(_) => ready(Err(warp::reject::not_found())),
             }
 }
-
-fn response_handler(req: Dummy) -> warp::reply::Json {
-    let result = Response {
-        data: get_data_from_db(req.path),
-        method: req.method.to_string(),
-    };
-
-    warp::reply::json(&result)
-    //format!("{} on this: {}", req.method, req.path) // Method invokation for db here
+// Should we even use 2 separate functions for request and response handling?
+fn response_handler(req: Dummy, paths: Vec<String>) -> Result<warp::reply::Json, warp::Rejection> {
+    if paths.contains(&req.path) {
+                match req.method {
+                    http::Method::GET => methods::get_request(req.path),
+                    http::Method::POST => methods::post_request(req.path),
+                    http::Method::PUT => methods::put_request(req.path),
+                    _ => Err(warp::reject::not_found()),
+                }
+            } else {
+                Err(warp::reject::not_found())
+            }
+    // let result = Response {
+    //     data: get_data_from_db(req.path),
+    //     method: req.method.to_string(),
+    // };
 }
 
-fn get_data_from_db(path: String) -> String {
-    format!("Data for path: {}", path)
-}
+// Code found on StackOverflow to handle rejections globally
+// We should probably use something like it in the future 
+//
+// fn handle_rejection(err: Rejection) -> Result<impl Reply, std::convert::Infallible> {
+//     if err.is_not_found() {
+//         Ok(reply::with_status("NOT_FOUND", StatusCode::NOT_FOUND))
+//     } else if let Some(e) = err.find::<AppErr>() {
+//         Ok(reply::with_status(e.reason.as_str(), StatusCode::BAD_REQUEST))
+//     } else {
+//         eprintln!("unhandled rejection: {:?}", err);
+//         Ok(reply::with_status(
+//             "INTERNAL_SERVER_ERROR",
+//             StatusCode::INTERNAL_SERVER_ERROR,
+//         ))
+//     }
+// }
 
 #[derive(serde::Serialize)]
 struct Response {
