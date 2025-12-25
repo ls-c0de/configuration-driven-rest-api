fb71027501398c7a12d3fb7670ff71c0506e4360
ls-c0de <137232009+ls-c0de@users.noreply.github.com>
2025-12-25 21:01:53 +0100

    Minor tweaks

--- src/db/migrations.rs
+++ src/db/migrations.rs
@@ -12,13 +12,13 @@ pub async fn start_migrations(db: Pool<Sqlite>) {
 
     // Apply migrations
     for model in config.model {
-        create_tables(db.clone(), model.tablename, model.fields).await.unwrap(); // Clone needed?
+        create_table(db.clone(), model.tablename, model.fields).await.unwrap(); // Clone needed?
     }
 }
 
 // Since sqlx does not support dynamic table creation, we build the query manually
 // No Risk of SQL Injection here, since table names and fields are controlled by the provided yaml file
-pub async fn create_tables(db: Pool<Sqlite>, table_name: String, fields: Vec<Fields<String>>) -> Result<(), sqlx::Error> {
+pub async fn create_table(db: Pool<Sqlite>, table_name: String, fields: Vec<Fields<String>>) -> Result<(), sqlx::Error> {
     if !table_name.chars().all(|c| c.is_alphanumeric() || c == '_') {
         panic!("Ungültiger Tabellenname");
     }
--- src/db/queries/dummy.rs
+++ src/db/queries/dummy.rs
@@ -1,13 +1,16 @@
+#[allow(dead_code)]
 pub fn get_element_from_db(path: String) -> String {
     // Dummy implementation, replace with actual database logic
     format!("Data for path: {}", path)
 }
 
+#[allow(dead_code)]
 pub fn save_element_to_db(path: String, data: String) -> String {
     // Dummy implementation, replace with actual database logic
     format!("Saved data for path: {} with data: {}", path, data)
 }
 
+#[allow(dead_code)]
 pub fn connect_to_db(connection_string: String) -> bool {
     // Dummy implementation, replace with actual database connection logic
     println!("Connecting to database with connection string: {}", connection_string);
